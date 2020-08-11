1235372de00bac2372464affabc16aa3871b9f29
Riazul Islam <ri64627@gmail.com>
2020-08-11 10:16:56 +0600

    added video changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,37 @@
-(function($) {
+(function ($) {
   "use strict";
 
-  jQuery(document).ready(function($) {
+  jQuery(document).ready(function ($) {
     // AOS init
     AOS.init({
-      once: true
-    })
+      once: true,
+    });
+    $("body").scrollspy({ target: "#navbarSupportedContent" });
 
+    // Smooth Scroll
+    var scroll = new SmoothScroll('a[href*="#"]', {
+      offset: 135,
+    });
+    //navbar Collapse
+    $(".navbar-collapse a").click(function () {
+      $(".navbar-collapse").collapse("hide");
+    });
+    $(".modal").each(function () {
+      var src = $(this).find("iframe").attr("src");
 
+      $(this).on("click", function () {
+        $(this).find("iframe").attr("src", "");
+        $(this).find("iframe").attr("src", src);
+      });
+    });
+  });
+
+  $(document).scroll(function () {
+    var y = $(this).scrollTop();
+    if (y > 800) {
+      $(".scroll-to-top").addClass("active");
+    } else {
+      $(".scroll-to-top").removeClass("active");
+    }
   });
 })(jQuery);
